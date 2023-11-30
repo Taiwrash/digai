01909c212f53af485c857ecd23d9e9c417312cf0
Rasheed Mudasiru <rasheedrtm1@gmail.com>
2023-11-30 12:36:01 +0100

    playground responsive

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -12,28 +12,32 @@ const Playground = () => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-200 px-4 sm:px-0">
-            <form onSubmit={handleSubmit} className="w-full">
-                <div className="flex flex-col sm:flex-row items-center mb-6">
-                    <div className="w-full sm:w-1/3 mb-2 sm:mb-0">
-                        <label className="block text-gray-500 font-bold mb-1 pr-4" htmlFor="inline-full-name">
+        <div className="flex items-center justify-center h-screen bg-gray-200 sm:p-6">
+            <form onSubmit={handleSubmit} className="w-full max-w-sm">
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
                             Email or Domain
                         </label>
                     </div>
-                    <div className="w-full sm:w-2/3">
+                    <div className="md:w-2/3 bg-gray-200">
                         <input
-                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            className="bg-gray-100 appearance-none border-2 border-[#314a64]-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                             id="inline-full-name"
                             type="text"
                             value={input}
+                            placeholder='Email Address  Please'
                             onChange={(e) => setInput(e.target.value)}
                         />
                     </div>
                 </div>
-                <div className="flex justify-end">
-                    <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                        Generate Records
-                    </button>
+                <div className="md:flex md:items-center">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button className="shadow bg-[#314a64] hover:bg-[#314a64] focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                            Analyze Now
+                        </button>
+                    </div>
                 </div>
             </form>
         </div>
